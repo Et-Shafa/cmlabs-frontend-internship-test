a5b7aabb85f00d3b78818f1f3172fdeaba23eee4
Et-Shafa <naadiramalaika.atsiila@gmail.com>
2024-10-20 08:06:58 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function() {
+    $("section").load("Home.html");
+    $("header").load("Header.html");
+  });
+
+// $('#link').click(function() {
+//     window.history.pushState('obj', 'newtitle', '/abc');
+//     return false;
+// });
